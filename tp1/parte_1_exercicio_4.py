c1aa87e45a3cbec98ffc8512497b9a250c9005c4
juliatlrc <julia.tallcaroline@outlook.com>
2025-03-30 18:12:03 -0300

    fix exercicio 4

--- tp1/parte_1_exercicio_4.py
+++ tp1/parte_1_exercicio_4.py
@@ -11,41 +11,46 @@ class MinHeap:
     def _right_child(self, index):
         return 2 * index + 2
 
-    def _heapify_up(self, index):
-        while index > 0 and self.heap[self._parent(index)] > self.heap[index]:
-            self.heap[self._parent(index)], self.heap[index] = self.heap[index], self.heap[self._parent(index)]
-            index = self._parent(index)
-
     def _heapify_down(self, index):
         smallest = index
         left = self._left_child(index)
         right = self._right_child(index)
 
+
         if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
             smallest = left
 
+
         if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
             smallest = right
+
 
         if smallest != index:
             self.heap[smallest], self.heap[index] = self.heap[index], self.heap[smallest]
             self._heapify_down(smallest)
 
-    def insert(self, value):
-        self.heap.append(value)
-        self._heapify_up(len(self.heap) - 1)
-
     def pop(self):
         if len(self.heap) == 0:
             return None
+
+
         root = self.heap[0]
+
+
         self.heap[0] = self.heap[-1]
+
+
         self.heap.pop()
+
+
         self._heapify_down(0)
+
+        
         return root
 
-# Testando a MinHeap
+
 min_heap = MinHeap()
+
 
 min_heap.insert(10)
 min_heap.insert(5)
@@ -53,7 +58,8 @@ min_heap.insert(15)
 min_heap.insert(2)
 min_heap.insert(8)
 
-print("Heap após inserções:", min_heap.heap)
+print("Heap antes da remoção:", min_heap.heap)
 
-print("Removendo o menor elemento:", min_heap.pop())  
+
+print("Elemento removido:", min_heap.pop()) 
 print("Heap após remoção:", min_heap.heap)
